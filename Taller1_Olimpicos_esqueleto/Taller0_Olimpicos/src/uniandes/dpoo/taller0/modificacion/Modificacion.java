6d8d8b7d88e0ac042b6160dcbf1637ab0ae49096
Juan Camilo Beltran <111070585+Juancabel@users.noreply.github.com>
2023-01-31 20:11:42 -0500

    Add files via upload

---
+++ Taller1_Olimpicos_esqueleto/Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,18 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	public static void main(String[] args) {
+		try {
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+		} catch (IOException except){
+			System.err.println("Error Cargando los archivos");
+			except.printStackTrace();
+		}
+	}
+}
